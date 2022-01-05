829dd129fbb127c56e238f3dd8444c8cc00405ba
Janet <janet.hyunjkim@gmail.com>
2022-01-05 19:28:58 +0100

    backend launched

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    "mongodb+srv://hyunjkim:blackee!@cluster0.hrgs8.mongodb.net/test?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use((req, res) => {
+  res.json({ message: "Votre requête a bien été reçue !" });
+});
+
+module.exports = app;
